8a4333c79c8ba4199d82e3b7f7c6b5e11341ce45
Juho Pyykkonen <juho.pyykkonen@tuni.fi>
2020-12-02 09:03:50 +0200

    removed reduntant data arrays

--- exercises/07/statistics_app/Statistics.c
+++ exercises/07/statistics_app/Statistics.c
@@ -11,14 +11,29 @@ Author: Trinh Gia Huy
 
 
 #define ROW_LENGTH 60       // characters per row
-#define ARRAY_SIZE 8192     // maximun stored rows
 #define IRQ_LINES 16        // irq lines count in irqgen
+
+#define INPUT_FILE "/dev/irqgen"
 
 // flag for stdin read loop active
 int read_inputs = 1;    
 
 //row counter for readed rows
 int row = 0;
+
+// input file operator
+FILE *fp;
+
+//data stcture for storing the data in final form
+struct irq_data{
+    int line_number;
+    int number_of_events;
+    double total_latency;
+    double worst_latency;
+};
+
+// index of array represent the line number
+struct irq_data irq_data_array[IRQ_LINES];
 
 
 //SIGINT signal handler
@@ -29,10 +44,25 @@ void sig_handler(int signum){
 
 int main(int argc, char **argv)
 {
+
+
+
+    // default values to irq_data_array
+    for(int i=0; i<IRQ_LINES; i++){
+        irq_data_array[i].line_number = -1;
+        irq_data_array[i].total_latency = -1;
+        irq_data_array[i].number_of_events = -1;
+        irq_data_array[i].worst_latency=-1;
+    }
     
     signal(SIGINT, sig_handler);
     
-    char data[ARRAY_SIZE][ROW_LENGTH];
+
+    fp = fopen(INPUT_FILE, "r");
+    if(fp == NULL) {
+      perror("Error opening file");
+      return(-1);
+    }
 
 
     //readding input
@@ -42,117 +72,70 @@ int main(int argc, char **argv)
 
         char input[ROW_LENGTH];
 
-        
-         if( fgets(input,ROW_LENGTH,stdin) != NULL){
+         // some data read
+         if( fgets(input,ROW_LENGTH,fp) != NULL){
+            
+            char *pcb;
+
+            pcb = strtok (input,",");
+
+            int g = 0;
+
+            int luku;
+
+            // storing the variable after converting from string to int
+            int int_array[2];        
+
+            while (pcb != NULL)
+            {
+                luku = atoi(pcb);
+
+                // check atoi success and drop timestamp from data
+                if (luku != 0 || g<2){
+                    int_array[g] = luku;
+                    g++;
+                }
+                pcb = strtok (NULL, ",");
+            }
+
+            int line_nbr = int_array[0];
+            int latency = int_array[1];
+
+            // set line number
+            if (irq_data_array[line_nbr].line_number == -1){
+                irq_data_array[line_nbr].line_number = line_nbr;
+            }
+
+            // set events number counter
+            if (irq_data_array[line_nbr].number_of_events == -1){
+                irq_data_array[line_nbr].number_of_events = 1;
+
+            } else {
+                irq_data_array[line_nbr].number_of_events++;
+            }
+
+            // set total latency
+            if (irq_data_array[line_nbr].total_latency == -1){
+                irq_data_array[line_nbr].total_latency = latency;
+
+            } else {
+                irq_data_array[line_nbr].total_latency += latency;
+            }
+
+            // set worst latency
+            if (irq_data_array[line_nbr].worst_latency == -1){
+                irq_data_array[line_nbr].worst_latency = latency;
+            } else if (latency > irq_data_array[line_nbr].worst_latency){
+                irq_data_array[line_nbr].worst_latency = latency;
+            } 
   
-            if (row < ARRAY_SIZE){
-                strcpy(data[row], input);
-                row++; 
-            }       
+             
         }
     }
+
+    fclose(fp);
 
     
-    //convertig chars in array to ints in array
-    //using strtok-function to split char arrays from ","
-
-    // array holding integer values
-    /*
-    [0] line number
-    [1] latency
-    [2] timestamp
-    */
-    int int_array[ARRAY_SIZE][3];
-
-    for (int i = 0; i<row; i++){
-
-        char *pcb;
-
-        pcb = strtok (data[i],",");
-
-        int g = 0;
-        
-
-        while (pcb != NULL)
-        {
-            int luku = atoi(pcb);
-
-            // check atoi success and drop timestamp from data
-            if (luku != 0 || g<2){
-                int_array[i][g] = luku;
-                g++;
-            }
-            pcb = strtok (NULL, ",");
-        } 
-    }
-
-  
-    //data stcture for storing the data in final form
-    struct irq_data{
-        int line_number;
-        int number_of_events;
-        double total_latency;
-        double worst_latency;
-    };
-
-    // index of array represent the line number
-    struct irq_data irq_data_array[IRQ_LINES];
-
-    // default values to irq_data_array
-    for(int i=0; i<IRQ_LINES; i++){
-        irq_data_array[i].line_number = -1;
-        irq_data_array[i].total_latency = -1;
-        irq_data_array[i].number_of_events = -1;
-        irq_data_array[i].worst_latency=-1;
-    }
-
-
-
-    // index throught int_array and parse data to struct-array
-    for (int a = 0; a<row; a++){
-
-        int rowdata[3];
-        // copy array
-        for(int i=0; i<3; i++){
-             rowdata[i] = int_array[a][i]; 
-        }  
-
-        int line_nbr = rowdata[0];
-        int latency = rowdata[1];
-
-        // set line number
-        if (irq_data_array[line_nbr].line_number == -1){
-            irq_data_array[line_nbr].line_number = line_nbr;
-        }
-
-        // set events number counter
-        if (irq_data_array[line_nbr].number_of_events == -1){
-            irq_data_array[line_nbr].number_of_events = 1;
-
-        } else {
-            irq_data_array[line_nbr].number_of_events++;
-        }
-
-        // set total latency
-        if (irq_data_array[line_nbr].total_latency == -1){
-            irq_data_array[line_nbr].total_latency = latency;
-
-        } else {
-            irq_data_array[line_nbr].total_latency += latency;
-        }
-
-        // set worst latency
-        if (irq_data_array[line_nbr].worst_latency == -1){
-            irq_data_array[line_nbr].worst_latency = latency;
-        } else if (latency > irq_data_array[line_nbr].worst_latency){
-            irq_data_array[line_nbr].worst_latency = latency;
-        }
-        
-        
-       
-        
-    }
-
 
     // final formal printing
 
